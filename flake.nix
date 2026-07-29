f1f2173e94a12900f2d950ec34ee4ea137b660b3
Justin Barclay <git@justinbarclay.ca>
2026-07-29 11:22:07 -0700

    feat: include updating the inventory during update-catalogue action

--- flake.nix
+++ flake.nix
@@ -9,27 +9,54 @@
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in
-    {
-      homeManagerModules.default = import ./default.nix;
-      homeManagerModules.agentic-skills = self.homeManagerModules.default;
 
-      devShells = forAllSystems (system:
+      scriptsFor = system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
           update-skills = pkgs.writeScriptBin "update-skills" (
             builtins.replaceStrings
               [ "#!/usr/bin/env nu" ]
               [ "#!${pkgs.nushell}/bin/nu" ]
               (builtins.readFile ./update-skills.nu)
           );
+
+          # Runs the generate-inventory block in INVENTORY.org and saves the
+          # buffer. The block itself is the generator; this only executes it.
+          update-inventory = pkgs.writeShellApplication {
+            name = "update-inventory";
+            runtimeInputs = [ pkgs.emacs-nox ];
+            text = ''
+              emacs -Q --batch -l org \
+                --eval '(setq org-confirm-babel-evaluate nil)' \
+                --visit="''${1:-INVENTORY.org}" \
+                --eval '(progn (org-babel-execute-buffer) (save-buffer))'
+            '';
+          };
+        };
+    in
+    {
+      homeManagerModules.default = import ./default.nix;
+      homeManagerModules.agentic-skills = self.homeManagerModules.default;
+
+      packages = forAllSystems scriptsFor;
+
+      apps = forAllSystems (system:
+        nixpkgs.lib.mapAttrs
+          (name: pkg: {
+            type = "app";
+            program = "${pkg}/bin/${name}";
+          })
+          (scriptsFor system));
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
         in
         {
           default = pkgs.mkShell {
-            packages = [
-              pkgs.nushell
-              update-skills
-            ];
+            packages = [ pkgs.nushell ] ++ builtins.attrValues (scriptsFor system);
           };
         });
     };
